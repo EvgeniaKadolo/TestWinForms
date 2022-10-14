a9d243bc24d248d6fdfff18a9aa453225df714de
«Evgenia» <ekadolo@mail.ru>
2022-10-15 00:21:27 +0500

    Initial commit

---
+++ TestWinForms/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics graphics;
+        List<Rectangle> rectangles = new List<Rectangle>();
+        List<Rectangle> rectanglesToRemove = new List<Rectangle>();
+        int count = 1;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics = CreateGraphics();
+
+            Rectangle rectangle = Rectangle.GetRandomRectangle();
+            
+            graphics.FillRectangle(new SolidBrush(rectangle.Color), rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+
+            foreach (Rectangle r in rectangles)
+            {
+                if (rectangle.Intersects(r))
+                {
+                    rectanglesToRemove.Add(r);
+                }
+            }
+
+            rectangles.Add(rectangle);
+
+            if (count % 5 == 0 && rectanglesToRemove.Count != 0)
+            {
+                foreach (Rectangle r in rectanglesToRemove)
+                {
+                    rectangles.Remove(r);
+                }
+
+                graphics.Clear(BackColor);
+                rectanglesToRemove.Clear();
+
+                foreach (Rectangle r in rectangles)
+                {
+                    graphics.FillRectangle(new SolidBrush(r.Color), r.X, r.Y, r.Width, r.Height);
+                }
+            }
+            count++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                button1.Text = "Start";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+        }
+    }
+}
---
+++ TestWinForms/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+
+namespace TestWinForms
+{
+    public class Rectangle
+    {
+        public float X { get; }
+        public float Y { get; }
+        public float Width { get; }
+        public float Height { get; }
+        public Color Color { get; }
+
+        public Rectangle(float x, float y, float width, float height, Color color)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Color = color;
+        }
+
+        public static Rectangle GetRandomRectangle()
+        {
+            Random random = new Random();
+
+            int width = random.Next(10, 250);
+            int height = random.Next(10, 150);
+
+            int x = random.Next(0, 500);
+            int y = random.Next(0, 300);
+
+            Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+
+            return new Rectangle(x, y, width, height, color);
+        }
+
+        public bool Intersects(Rectangle rectangle)
+        {
+            return !(X + Width < rectangle.X ||
+                     X > rectangle.X + rectangle.Width ||
+                     Y > rectangle.Y + rectangle.Height ||
+                     Y + Height < rectangle.Y);
+        }
+    }
+}
